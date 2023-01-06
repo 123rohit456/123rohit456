d2ec1dd64abceab7e6d4ddf2069db86e5e81577b
123rohit456 <114637725+123rohit456@users.noreply.github.com>
2023-01-06 08:35:40 +0530

    Update FDSMINIPROJECT.C

--- FDSMINIPROJECT.C
+++ FDSMINIPROJECT.C
@@ -2,32 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Defining a structure for the node//
 struct contact
 {
     char name[20];
-    char contact[10];
-    char gmail[20];
+    char contact[12];
+    char gmail[30];
     char fb_id[20];
     struct contact *next;
 };
+
+
 int i = 1, size = 0;
+
 struct contact *head;
-void insert_contact();
-void search_contact();
-struct contact * delete_contact();
-void display_contact();
+
+void insert_contact();//Function to insert contact// 
+void search_contact();//Function to search contact//
+void delete_contact();//Function to delete contact//
+void display_contact();//Function to display contact//
 
 void main()
 {
     struct contact *head = NULL;
     char c[40];
     int ch, y;
-    printf("\n\n\t !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    printf("\n\t !!!!!!WELCOME TO OUR PHONE BOOK!!!!!!");
-    printf("\n\t !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+    printf("\n\n\t------------------------------------");
+    printf("\n\t------WELCOME TO OUR PHONE BOOK------");
+    printf("\n\t-----------------------------------\n");
     do
     {
-        printf("1.Insert contact\n2.Search contact\n3.Delete contact\n4.Display contact\n");
+        printf("-----------------------\n[1]Insert contact\n[2]Search contact\n[3]Delete contact\n[4]Display contact\n-----------------------\n");
         printf("Enter choice : ");
         scanf("%d", &ch);
         switch (ch)
@@ -39,23 +44,33 @@ void main()
             search_contact();
             break;
         case 3:
-            head=delete_contact();
+            delete_contact();
             break;
         case 4:
             display_contact();
             break;
         default:
-            printf("\n\t Invalid choice!!Try again!!!");
+            printf("\n\t------------Invalid choice-----------");
             break;
         }
-        printf("\nDo you wish to continue(0/1)\n");
+        printf("\n--------Do you wish to continue(0/1)--------\n");
         scanf("%d", &y);
     } while (y == 1);
 }
 
 void insert_contact()
 {
+    //Size of the contact book list is 20//
+    if (size == 20)
+    {
+        printf("\n\n\n\t --------Phone Memory is Full------------");
+        printf("\n\tIf you want to add more contact,you have to delete some contact from your list...\n\n");
+        return;
+    }
+    else
+    {
     struct contact *new = (struct contact*)malloc(sizeof(struct contact));
+    printf("------------------------------------------------\n");
     printf("\n\n\n\tContact Name : ");
     scanf(" %s", new->name);
     printf("\n\tContact Number : ");
@@ -64,17 +79,9 @@ void insert_contact()
     scanf(" %s", new->gmail);
     printf("\n\tFacebook account : ");
     scanf(" %s", new->fb_id);
+    printf("\n------------------------------------------------");
     new->next = NULL;
     size++;
-
-    if (size == 20)
-    {
-        printf("\n\n\n\tPhone Memory is Full");
-        printf("\n\tIf you want to add more contact,you have to delete some contact from your list...\n\n");
-        return;
-    }
-    else
-    {
         if (head == NULL)
         {
             head = new;
@@ -97,9 +104,9 @@ void insert_contact()
 
 void search_contact()
 {
-    char d[10];
-    printf("\n\n\n\t Contact number : ");
-    scanf(" %s", d);
+    char d[20];
+   
+ 
 
     if (head == NULL)
     {
@@ -108,15 +115,19 @@ void search_contact()
     }
     else
     {
+        printf("\n\n\n\t Contact name : ");
+        scanf(" %s", d);
         struct contact *a = head;
         while (a != NULL)
         {
-            if (!strcmp(a->contact,d))
-            {
+            if (!strcmp(a->name,d))//This command is true when both the strings are equal//
+            {  
+                printf("------------------------------------------------\n");
                 printf("\n\n\tContact name : %s ", a->name);
                 printf("\n\tContact number : %s", a->contact);
                 printf("\n\tGmail id : %s", a->gmail);
                 printf("\n\tFacebook account : %s", a->fb_id);
+                printf("\n------------------------------------------------");
                 return;
             }
             a = a->next;
@@ -124,20 +135,21 @@ void search_contact()
         printf("\n\n\n\tThis contact does not exist in the list!!!");
     }
 }
-
-struct contact * delete_contact()
+void delete_contact()
 {
     char d[10];
-    printf("\n\n\n\t Contact number : ");
-    scanf(" %s", d);
-
+   
+    struct contact *temp=head,*temp1;
     if (head == NULL)
     {
         printf("\n\tNo Contact Exist in this Phone book List!\n\n");
     }
     else
     {
-        if (!strcmp(head->contact,d))
+        printf("\n\n\n\t Contact name : ");
+        scanf(" %s", d);
+       
+        if (!strcmp(head->name,d))
         {
             struct contact *a = head;
             head=head->next;
@@ -146,27 +158,25 @@ struct contact * delete_contact()
         }
         else
         {
-            struct contact *a=head;
-            struct contact *b=head->next;
-
-            while (b->next!= NULL)
+            while(strcmp(temp->name,d)!=0)//This command is true when both the strings are equal
             {
-                if (!strcmp(b->contact,d))
-                {
-                    a->next = b->next;
-                    free(b);
-                    size--;
-                    break;
-                }
-                a=a->next;
-                b = b->next;
+                temp1=temp;
+                temp=temp->next;
             }
-            
-            printf("\n\t\tInvalid Contact!!!\n\n");
+            if(temp->next==NULL)// if the contact to be deleted is the last one//
+            {
+                temp1->next=NULL;
+                free(temp);
+            }
+            else//if the contact is somewhere in between//
+            {
+                temp1->next=temp->next;
+                free(temp);
+            }
         }
     }
-    return head;
 }
+
 
 void display_contact()
 {
@@ -180,15 +190,15 @@ void display_contact()
         struct contact *a = head;
         while (a != NULL)
         {
-
+            printf("------------------------------------------------\n");
             printf("\n\n\t%d.Contact name : %s", i, a->name);
             printf("\n\tContact number : %s", a->contact);
             printf("\n\tGmail id : %s", a->gmail);
             printf("\n\tFacebook account : %s", a->fb_id);
+            printf("\n------------------------------------------------");
             a = a->next;
             i++;
         }
         i = 1;
         return;
     }
-}
