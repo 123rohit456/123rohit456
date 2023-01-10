7d9ea73230b8a35f864b97b13d3c80fbec0699dc
123rohit456 <114637725+123rohit456@users.noreply.github.com>
2023-01-10 11:48:20 +0530

    Update Assignment_1.c

--- Assignment_1.c
+++ Assignment_1.c
@@ -68,21 +68,74 @@ void multiplication()
     for(j=0;j<n;j++)
     scanf("%d",(q+((i*m)+j)));
     
-    int e[a][n],*r;
-    r=&e[0][0];
+    int e[a][n];
     for(i=0;i<a;i++)
     for(j=0;j<n;j++)
-    *(r+((i*a)+j))==0;
+    e[i][j]=0;
     
     for(i=0;i<a;i++)
-    for(i=0;i<n;i++)
+    for(j=0;j<n;i++)
     for(k=0;k<n;k++)
-    *(r+((i*a)+j))= *(r+((i*a)+j))
+    e[i][j]+=(*(p+((i*a)+k)))*(*(q+((k*n)+j)));
     
+    printf("The multiplication of 2 matrices is : \n");
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d",d[i][j]);
+            printf("\t");
+        }
+        printf("\n");
     }
-    
-    
+    }
 }
+
+void saddle()
+{
+    int a[3][3],*p,i,j,min,max,s[2][2],*r,k;
+    p=&a[0][0];
+    r=&s[0][0];
+    printf("Enter matrix elements : ");
+    for(i=0;i<3;i++)
+    for(j=0;j<3;j++)
+    scanf("%d",(p+((i*3)+j)));
+    
+    for(i=0;i<3;i++)
+    {
+        min=*(p+((i*3)+0));
+        for(j=0;j<3;j++)
+        {
+            if(min>=*(p+((i*3)+j)))
+            {
+                min=*(p+((i*3)+j));
+                *(r+((0*2)+0))=i;
+                *(r+((0*2)+1))=j;
+            }
+        }
+        j=*(r+((0*3)+1));
+        max=*(p+0*3+j);
+        
+        for(k=0;k<3;k++)
+        {
+            if(max<=*(p+((k*3)+j)))
+            {
+                max=*(p+((k*3)+j));
+                *(r+((1*2)+0))=k;
+                *(r+((1*2)+1))=j;
+            }
+        }
+        
+        if(min==max)
+        {
+            if(*(r+((0*2)+0))==*(r+((1*2)+0)) && *(r+((0*2)+1))==*(r+((1*2)+1)))
+            {
+                printf("Saddle point is at row %d and column %d which is %d \n",*(r+((0*2)+0)),*(r+((0*2)+1)),min);
+            }
+        }
+    }
+}
+
 
 int main()
 {   int ch,y;
@@ -96,8 +149,8 @@ int main()
            addition();
            case 2:
            multiplication();
-           /*case 3:
-           saddle();*/
+           case 3:
+           saddle();
        }printf("Do you wish to continue(0/1) ");
        scanf("%d",&y);
     }while(y==1);
